bc562e1ae889b8cdc9fc5531e39f3ff8bfac0fa1
TPt480 <159018843+Michael668875@users.noreply.github.com>
2026-09-01 13:34:31 +1000

    added temp listings data

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
+from .models import TempSummary
 
 # Create your views here.
 
 class Home(TemplateView):
     template_name = "listings/home.html"
 
-class ListingsView(TemplateView):
+class ListingsView(ListView):
+    model = TempSummary
     template_name = "listings/listings.html"
+
+    context_object_name = "listings"
